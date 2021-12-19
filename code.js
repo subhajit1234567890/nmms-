b6b56553a6c4594c6daffc7a65c82b359c7b6134
subhajit1234567890 <93361025+subhajit1234567890@users.noreply.github.com>
2021-12-19 10:28:05 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,77 @@
+
+onEvent("MAT", "click", function( ) {
+  setScreen("screen2");
+});
+
+
+
+onEvent("mat2013", "click", function( ) {
+  open("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/viewer.html?pdfurl=https%3A%2F%2Fscholarships.wbsed.gov.in%2Freadwrite%2Finstruction%2FNMMS_2013.pdf&clen=4583971&chunk=true");
+});
+
+
+
+onEvent("mat2014", "click", function( ) {
+  open("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/viewer.html?pdfurl=https%3A%2F%2Fscholarships.wbsed.gov.in%2Freadwrite%2Finstruction%2FNMMS_2014.pdf&clen=5834346&chunk=true");
+});
+
+
+onEvent("mat2015", "click", function( ) {
+  open("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/viewer.html?pdfurl=https%3A%2F%2Fscholarships.wbsed.gov.in%2Freadwrite%2Finstruction%2FNMMS_2015.pdf&clen=5574226&chunk=true");
+});
+
+
+onEvent("mat2016", "click", function( ) {
+  open("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/viewer.html?pdfurl=https%3A%2F%2Fscholarships.wbsed.gov.in%2Freadwrite%2Finstruction%2FNMMS_2016.pdf&clen=5912289&chunk=true");
+});
+
+
+
+onEvent("mat2017", "click", function( ) {
+  open("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/viewer.html?pdfurl=https%3A%2F%2Fscholarships.wbsed.gov.in%2Freadwrite%2Finstruction%2FNMMS_2017.pdf&clen=2320802&chunk=true");
+});
+
+
+onEvent("mat2018", "click", function( ) {
+  open("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/viewer.html?pdfurl=https%3A%2F%2Fscholarships.wbsed.gov.in%2Freadwrite%2Finstruction%2FNMMS_2017.pdf&clen=2320802&chunk=true");
+});
+
+onEvent("mat2019", "click", function( ) {
+  open("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/viewer.html?pdfurl=https%3A%2F%2Fscholarships.wbsed.gov.in%2Freadwrite%2Finstruction%2FNMMSE_2019_EXAM_MAT_SAT.pdf&clen=4024038&chunk=true");
+});
+
+
+onEvent("mat2020", "click", function( ) {
+  open("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/viewer.html?pdfurl=https%3A%2F%2Fscholarships.wbsed.gov.in%2Freadwrite%2Finstruction%2FNMMSE_2020_MAT_SAT.pdf&clen=2123198&chunk=true");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
